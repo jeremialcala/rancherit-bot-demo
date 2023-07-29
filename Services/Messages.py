f2a06d0bf82d613ea819b2eb604a3f9cfb164eee
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-29 13:47:55 -0500

    - Handling some errors.

--- Services/Messages.py
+++ Services/Messages.py
@@ -77,7 +77,7 @@ def process_messages(msg: Messaging):
         if user["tyc"] is False:
             send_tyc(sender, user)
             return
-
+        log.info(message.to_json())
         if message.attachments is None:
             concepts = get_concept(message.text)
             log.info(concepts)
