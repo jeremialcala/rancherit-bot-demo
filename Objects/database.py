f01fe266db15259e91a441f4e895394721593292
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-29 11:34:49 -0500

    - Fixing some env variables

--- Objects/database.py
+++ Objects/database.py
@@ -5,10 +5,10 @@ from Constants import *
 
 
 class Database(Object):
-    def __init__(self, schema=os.environ.get(SCHEMA)):
+    def __init__(self, schema=os.environ[SCHEMA]):
         super().__init__()
         self.schema = schema
-        self.client = pymongo.MongoClient(os.environ.get(MONGO))
+        self.client = pymongo.MongoClient(os.environ[MONGO])
         self.db = self.client[schema]
 
     def get_schema(self):
