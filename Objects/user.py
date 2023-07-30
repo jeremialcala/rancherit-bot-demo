bb4e165d6b9482e57a81458e508d30463d238dc2
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-29 20:11:26 -0500

    - Fixing User object issue with the ObjectID.

--- Objects/user.py
+++ Objects/user.py
@@ -1,12 +1,10 @@
 
 from .object import Object
-from bson import ObjectId
 
 
 class User(Object):
     def __init__(self, first_name, last_name, profile_pic, id, tyc=False, register_status=0, operation_status=0):
         super().__init__()
-        self._id = ObjectId()
         self.first_name = first_name
         self.last_name = last_name
         self.profile_pic = profile_pic
